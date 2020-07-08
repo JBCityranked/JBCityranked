4639c56c4719001400a83f8e40c7aa155fd1aca4
Josh Brown <josh@cityranked.com>
2020-07-08 11:17:43 -0700

    Add menu page

--- a-universal-style.php
+++ a-universal-style.php
@@ -3,3 +3,22 @@
  * Plugin Name: A Univeral Style
  * Description: A universal syle panel
  */
+ /**
+  * Register a custom menu page.
+  */
+ function wpdocs_register_my_custom_menu_page(){
+     add_menu_page(
+         'Universal Style Sheet',
+         'Universal Style Sheet',
+         'manage_options',
+         'cr-universal-stylesheet',
+         'universal_stylesheet_render',
+         plugins_url( 'myplugin/images/icon.png' ),
+         6
+     );
+ }
+ add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
+
+function universal_stylesheet_render() {
+  echo 'Universal Style Sheet Render';
+}
