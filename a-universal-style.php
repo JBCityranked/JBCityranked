c136848e2b58d53506dbd75116f1cd5a5c2d9690
Josh Brown <josh@cityranked.com>
2020-08-19 10:48:53 -0700

    Wk4 - add beginning of Ajax

--- a-universal-style.php
+++ a-universal-style.php
@@ -19,8 +19,50 @@
  }
  add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
 
+/**
+ * Render the Style Admin Panel
+ * @return void
+ */
 function universal_stylesheet_render() {
   echo 'Universal Style Sheet Render <br>';
 
-  echo '<textarea name="" id="" cols="30" rows="10"></textarea>';
+  echo '<textarea name="universal_stylesheet" id="universal_stylesheet" cols="80" rows="10"></textarea></br>';
+
+  echo '<button id="saveUniversal">Save Styles</button>';
+}
+
+add_action( 'wp_ajax_save_universal', 'save_universal' );
+
+/**
+ * Create Ajax to Save our Universal Styles
+ * @return void
+ */
+function save_univeral_ajax_enqueue() {
+    $localization_array = array(
+      '_nonce'  =>  wp_create_nonce(),
+      'action'  =>  'save_universal',
+      'url'     =>  site_url()
+    );
+
+    wp_enqueue_script( 'universal-ajax', plugins_url('/js/save_universal.js', __FILE__ ), array( 'jquery' ) );
+}
+add_action( 'admin_enqueue_scripts', 'save_univeral_ajax_enqueue' );
+
+
+/**
+ * Ajax Call back - Where we save our Styles
+ * @return [type] [description]
+ */
+function save_universal_ajax_callback() {
+	global $wpdb; // this is how you get access to the database
+
+	$sent_stylees = $_POST['styles'];
+
+	// Update our options
+
+  // Return Success
+
+  echo 'Success';
+
+	wp_die(); // this is required to terminate immediately and return a proper response
 }
---
+++ js/save_universal.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+
+  $('#saveUniversal').on('click', function(){
+    console.log('saving styles');
+  })
+
+	// var data = {
+	// 	'action': 'save_universal',
+	// 	// 'whatever': ajax_object.we_value      // We pass php values differently!
+	// };
+	// We can also pass the url value separately from ajaxurl for front end AJAX implementations
+	// jQuery.post(ajax_object.ajax_url, data, function(response) {
+	// 	alert('Got this from the server: ' + response);
+	// });
+});
